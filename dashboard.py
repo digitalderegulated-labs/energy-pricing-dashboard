6e7584204c91c67d253b5fe68e61a38a437d0597
Casey Min <caseymin.me@gmail.com>
2026-03-04 20:40:29 -0500

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -1,21 +1,34 @@
 import streamlit as st
 import pandas as pd
 import requests
+import plotly.express as px
 
-st.title("Energy API Debug")
+st.set_page_config(layout="wide")
+
+st.title("⚡ U.S. Energy Market Dashboard")
 
 API_KEY = st.secrets["EIA_API_KEY"]
 
-url = f"https://api.eia.gov/v2/electricity/rto/region-data/data/?api_key={API_KEY}&frequency=hourly&data[0]=value"
+url = f"https://api.eia.gov/v2/electricity/rto/region-data/data/?api_key={API_KEY}&frequency=hourly&data[0]=value&start=2024-01-01"
 
 response = requests.get(url)
 
-data = response.json()
+data = response.json()["response"]["data"]
 
-st.write("RAW API RESPONSE")
-st.write(data)
+df = pd.DataFrame(data)
 
-df = pd.DataFrame(data["response"]["data"])
+df["period"] = pd.to_datetime(df["period"])
 
-st.write("DATAFRAME")
-st.write(df)
+st.subheader("Wholesale Electricity Prices")
+
+fig = px.line(
+    df,
+    x="period",
+    y="value",
+    color="region",
+    title="Wholesale Electricity Prices by Region"
+)
+
+st.plotly_chart(fig, use_container_width=True)
+
+st.dataframe(df)
